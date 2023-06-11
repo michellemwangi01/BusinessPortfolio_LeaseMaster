09e9b2e94a9c7dc29bc08b418bf9ae2c0c76b9b2
michellemwangi01 <michellemwangim@gmail.com>
2023-06-11 22:49:25 +0300

    fix cards

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const teamMembers = document.querySelectorAll('.memberSocials');
+const teamMembers = document.querySelector('#memberSocials');
 const memberSocials =  document.getElementsByClassName("memberSocials");
 
 console.log(teamMembers);
 
-for (element in teamMembers){
-  console.log(element);
-}    
+
+// for (element in teamMembers){
+//   let textInImage = teamMembers[0].textContent;
+//   textInImage.textContent = "hovered";
+// }  
+
+teamMembers.addEventListener("mouseover", (e)=>{
+  let textInImage = teamMembers.innerHTML;
+  textInImage.innerHTML = "hovered";
+})
+
+  
