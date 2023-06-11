a5768fda7aa8c4bcb53aad9baa93b7b322a5b281
michellemwangi01 <michellemwangim@gmail.com>
2023-06-11 22:14:32 +0300

    footer and form added

---
+++ script.js
@@ -0,0 +1,8 @@
+const teamMembers = document.querySelectorAll('.memberSocials');
+const memberSocials =  document.getElementsByClassName("memberSocials");
+
+console.log(teamMembers);
+
+for (element in teamMembers){
+  console.log(element);
+}    
